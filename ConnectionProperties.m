7a280f382f4af0667123e2b0b0d7011a6dc21acc
Michael <aristarkhov.m@gmail.com>
2020-07-27 00:20:36 +0200

    Extracting connection properties from hardcoded values into constants

--- ConnectionProperties.m
+++ ConnectionProperties.m
@@ -1,5 +1,7 @@
 classdef ConnectionProperties
-    properties (Constant = true)
-        inet_addr = 'https://research.stlouisfed.org/fred2/'
+    properties (Constant)
+        url_cookies = 'https://finance.yahoo.com/quote/'
+        url_quotes  = 'https://query1.finance.yahoo.com/v7/finance/download/'
+        user_agent  = 'Mozilla/5.0'
     end
 end
