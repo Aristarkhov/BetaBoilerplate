7b40fda5512fc7e24c76b692d44d610931f7ab5e
Michael <aristarkhov.m@gmail.com>
2020-07-28 10:27:05 +0200

    optimal MV portfolio and correlation map

--- script_mv_port.m
+++ script_mv_port.m
@@ -1,6 +1,6 @@
 load symbols.mat
 date1 = '30-Jul-2015';
-date2 = '4-Oct-2016';
+date2 = '4-Oct-2015';
 
 data=fetch_data_mapped_by_tickers(symbols, date1, date2);
 tt=extractTimeSeriesFromDataMap(data,'Close');
@@ -15,3 +15,9 @@ p = Portfolio('AssetMean', retMean, 'AssetCovar', retCov, 'AssetList', symbols);
 p = setDefaultConstraints(p);
 
 frt = estimateFrontier(p, 7);
+
+retCorr=corrcoef(returns);
+figure, heatmap(retCorr, 'Colormap', parula(5), 'ColorbarVisible' ,'on');
+ax=gca; ax.XData=symbols; ax.YData=symbols;
+
+
