80796d1cd0a7f7fd432fc605ba913d08caf7c77b
Michael <aristarkhov.m@gmail.com>
2020-07-28 13:38:32 +0200

    PCA target risk modeling

--- script_mv_port.m
+++ script_mv_port.m
@@ -1,6 +1,6 @@
 load symbols.mat
-date1 = '30-Jul-2015';
-date2 = '4-Oct-2015';
+date1 = '30-Jul-2016';
+date2 = '4-Oct-2018';
 
 data=fetch_data_mapped_by_tickers(symbols, date1, date2);
 tt=extractTimeSeriesFromDataMap(data,'Close');
@@ -18,6 +18,6 @@ frt = estimateFrontier(p, 7);
 
 retCorr=corrcoef(returns);
 figure, heatmap(retCorr, 'Colormap', parula(5), 'ColorbarVisible' ,'on');
-ax=gca; ax.XData=symbols; ax.YData=symbols;
+ax=gca; ax.XData=tt.Properties.VariableNames; ax.YData=tt.Properties.VariableNames;
 
 
