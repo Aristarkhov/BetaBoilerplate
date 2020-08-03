65b79c5014f17db50c63afcc05dd66465347fb2a
Michael <aristarkhov.m@gmail.com>
2020-08-03 19:14:13 +0200

    working in larger amount of data

--- script_mv_port.m
+++ script_mv_port.m
@@ -1,6 +1,6 @@
 load symbols.mat
-date1 = '30-Jul-2016';
-date2 = '4-Oct-2018';
+date1 = '30-Jul-2015';
+date2 = '4-Oct-2019';
 
 data=fetch_data_mapped_by_tickers(symbols, date1, date2);
 tt=extractTimeSeriesFromDataMap(data,'Close');
@@ -17,7 +17,7 @@ p = setDefaultConstraints(p);
 frt = estimateFrontier(p, 7);
 
 retCorr=corrcoef(returns);
-figure, heatmap(retCorr, 'Colormap', parula(5), 'ColorbarVisible' ,'on');
+figure('Name', 'Symbols correlation matrix'), heatmap(retCorr, 'Colormap', parula(5), 'ColorbarVisible' ,'on');
 ax=gca; ax.XData=tt.Properties.VariableNames; ax.YData=tt.Properties.VariableNames;
 
 
